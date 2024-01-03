9239976ce022790a90f41d18b21f7cf1742c80ac
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-03 13:24:11 +0200

    sending a PUT http request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,32 @@
 export default {
-    registerCoach(context, data) {
-        const coachData = {
-            id: context.rootGetters.userId,
-            firstName: data.first,
-            lastName: data.last,
-            description: data.desc,
-            hourlyRate: data.rate,
-            areas: data.areas
-        };
-        context.commit('registerCoach', coachData);
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      id: context.rootGetters.userId,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+
+    const response = await fetch(
+      `https://coachapp-5f863-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json();
+
+    if (!response.ok) {
+      // error ...
     }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
+  },
 };
