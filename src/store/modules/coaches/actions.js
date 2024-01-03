0fa45e7c64a8483b9a4f55f797a062124c46e1d2
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-03 13:47:11 +0200

    fetching coach data (GET http request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -29,4 +29,32 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coachapp-5f863-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // ...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lasttName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+
+    }
+
+    context.commit('setCoaches', coaches);
+  },
 };
