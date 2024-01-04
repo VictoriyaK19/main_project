d9c8458a3647d808b6c57ea14a50a6823bbf85c3
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-04 16:02:50 +0200

    sending coaching requests http requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,12 +1,49 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
             id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         };
+        const response = await fetch(`https://coachapp-5f863-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to sent request.');
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
+        newRequest.coachId = payload.coachId;
+
         context.commit('addRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId;
+        const response = await fetch(`https://coachapp-5f863-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch request.');
+            throw error;
+        }
+
+        const request = [];
+
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId: coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message
+            };
+            request.push(request);
+        }
+        context.commit("setRequests", request);
     }
 
 };
