ac35137f64c2f09d6a0a8d318c1172023332972d
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-04 16:06:48 +0200

    fixed

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,45 +1,45 @@
 <template>
-  <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
     <p>{{ error }}</p>
   </base-dialog>
   <section>
     <base-card>
       <header>
-        <h2>Request Received</h2>
+        <h2>Requests Received</h2>
       </header>
       <base-spinner v-if="isLoading"></base-spinner>
       <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
-          :email="req.userEmail" 
+          :email="req.userEmail"
           :message="req.message"
         ></request-item>
       </ul>
-      <h3 v-else>You haven't received any request yet!</h3>
+      <h3 v-else>You haven't received any requests yet!</h3>
     </base-card>
   </section>
 </template>
 
 <script>
-import RequestItem from "@/components/requests/RequestItem";
+import RequestItem from '../../components/requests/RequestItem.vue';
 
 export default {
   components: {
     RequestItem,
   },
-  date() {
+  data() {
     return {
       isLoading: false,
       error: null,
-    }
+    };
   },
   computed: {
     receivedRequests() {
-      return this.$store.getters["requests/requests"];
+      return this.$store.getters['requests/requests'];
     },
     hasRequests() {
-      return this.$store.getters["requests/hasRequests"];
+      return this.$store.getters['requests/hasRequests'];
     },
   },
   created() {
@@ -50,15 +50,15 @@ export default {
       this.isLoading = true;
       try {
         await this.$store.dispatch('requests/fetchRequests');
-      } catch (error){
+      } catch (error) {
         this.error = error.message || 'Something failed!';
       }
       this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   },
-  handleError() {
-    this.error = null;
-  }
 };
 </script>
 
@@ -78,5 +78,3 @@ h3 {
   text-align: center;
 }
 </style>
-
-
