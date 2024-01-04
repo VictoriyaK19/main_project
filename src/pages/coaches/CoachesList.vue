cdbe52c61d7ed6ab6195a1b432cf5304c01148e2
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-04 17:25:32 +0200

    adding route transitions

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <base-dialog :show="!!error" title="An error occured!" @close="handleError">
     <p>{{ error }}</p>
   </base-dialog>
@@ -30,6 +31,7 @@
       <h3 v-else>No coaches found.</h3>
     </base-card>
   </section>
+</div>
 </template>
 
 <script>
