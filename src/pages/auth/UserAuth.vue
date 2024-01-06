f19c618db85230a327b073fb0b18e4a11ca00279
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-06 13:50:15 +0200

    fixed

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,6 +1,6 @@
 <template>
   <base-card>
-    <form @sumbit.prevent="submitForm">
+    <form @submit.prevent="submitForm">
       <div class="form-control">
         <label for="email">E-Mail</label>
         <input type="email" id="email" v-model.trim="email" />
@@ -9,14 +9,11 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model.trim="password" />
       </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long).
-      </p>
+      <p
+        v-if="!formIsValid"
+      >Please enter a valid email and password (must be at least 6 characters long).</p>
       <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthForm"
-        >{{ switchModeButtonCaption }}</base-button
-      >
+      <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
     </form>
   </base-card>
 </template>
@@ -25,56 +22,55 @@
 export default {
   data() {
     return {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
       formIsValid: true,
-      mode: "login",
+      mode: 'login',
     };
   },
   computed: {
     submitButtonCaption() {
-        if (this.mode === 'login') {
-            return 'Login';
-        } else {
-            return 'Signup';
-        }
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Signup';
+      }
     },
     switchModeButtonCaption() {
-        if (this.mode === 'login') {
-            return 'Signup instead';
-        } else {
-            return 'Login instead';
-        }
-    }
+      if (this.mode === 'login') {
+        return 'Signup instead';
+      } else {
+        return 'Login instead';
+      }
+    },
   },
   methods: {
     submitForm() {
       this.formIsValid = true;
       if (
-        this.email === "" ||
-        !this.email.includes("@") ||
+        this.email === '' ||
+        !this.email.includes('@') ||
         this.password.length < 6
       ) {
         this.formIsValid = false;
         return;
       }
-      
+
       if (this.mode === 'login') {
         // ...
       } else {
         this.$store.dispatch('signup', {
-          emsil: this.email,
+          email: this.email,
           password: this.password,
         });
       }
-
     },
-    switchAuthForm() {
-        if (this.mode === 'login') {
-            this.mode = 'signup';
-        } else {
-            this.mode = 'login';
-        }
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
     },
   },
 };
