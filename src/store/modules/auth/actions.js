710c084703697e2014c246463427982d2f4b0512
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-06 17:04:49 +0200

    adding a login action & flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,30 @@
 export default {
-    login() {},
+    async login(context, payload) {
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyChe_sEonRPjkUVkGEYHxx6VjNs3iIz6zY', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(responseData);
+            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+            throw error;
+        }
+
+        console.log(responseData);
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiratopn: responseData.expiration
+        });
+
+    },
     async signup(context, payload) {
         const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyChe_sEonRPjkUVkGEYHxx6VjNs3iIz6zY', {
             method: 'POST',
@@ -23,6 +48,6 @@ export default {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiratopn: responseData.expiration
-        })
+        });
     }
 }
